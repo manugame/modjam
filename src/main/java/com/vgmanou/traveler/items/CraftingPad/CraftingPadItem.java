2fd5cf1be950b80ee598897bce65702c02a56c99
vgtom4 <52112041+vgtom4@users.noreply.github.com>
2021-02-15 22:47:37 +0100

    tu ne sais pas jouer à Paladium ? (corrigé)

--- src/main/java/com/vgmanou/traveler/items/CraftingPad/CraftingPadItem.java
+++ src/main/java/com/vgmanou/traveler/items/CraftingPad/CraftingPadItem.java
@@ -18,7 +18,7 @@ public class CraftingPadItem extends Item {
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
         if (!worldIn.isRemote()) {
-            worldIn.playSound((PlayerEntity)null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundInit.DELTA_SOUND.get(), SoundCategory.VOICE, 1.0F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
+            worldIn.playSound((PlayerEntity)null, playerIn.getPosX(), playerIn.getPosY(), playerIn.getPosZ(), SoundInit.DELTA_SOUND.get(), SoundCategory.VOICE, 1.0F, 1.0F);
             playerIn.openContainer(new SimpleNamedContainerProvider((id, playerInv, player) -> new CraftingPadContainer(id, playerInv, IWorldPosCallable.of(worldIn, player.getPosition())), new TranslationTextComponent("item.traveler.crafting_pad")));
             return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
         }
